3597bc36ce56c94a9596b57ce9ca8854f75a367b
Mateusz Madej <madej.mateusz.95@gmail.com>
2020-03-25 15:35:39 +0100

    first commit

---
+++ precache-manifest.5564c9f613ddabe2feeac1ce480ba563.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a083d8994e79887d36676f42c33953a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1f39c32f02b025a71d5",
+    "url": "/static/css/main.8c13fce2.chunk.css"
+  },
+  {
+    "revision": "baf5da3837852295e309",
+    "url": "/static/js/2.d370a313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d370a313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f39c32f02b025a71d5",
+    "url": "/static/js/main.73446546.chunk.js"
+  },
+  {
+    "revision": "7550990f541bd96a7e71",
+    "url": "/static/js/runtime-main.c1a580e6.js"
+  },
+  {
+    "revision": "76d64a2142b071e88f140c735e08bc3a",
+    "url": "/static/media/coldbg.76d64a21.jpeg"
+  },
+  {
+    "revision": "c43fed59857d1350e3f5ca35e629a5e2",
+    "url": "/static/media/warmbg.c43fed59.jpeg"
+  }
+]);
